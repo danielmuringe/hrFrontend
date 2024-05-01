54c8cce7d3fa4ff4d91a0389db4ad1f2c7fe5d75
DanielMuringe <danielmuringe@gmail.com>
2024-05-01 10:37:12 +0300

    ref: small changes in App.java

--- src/main/java/org/example/hrFrontend/App.java
+++ src/main/java/org/example/hrFrontend/App.java
@@ -1,6 +1,7 @@
 package org.example.hrFrontend;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 
 public class App {
@@ -14,22 +15,34 @@ public class App {
         }};
 
         JFrame emptyLoginNextPage = new JFrame() {{
-            setSize(720, 500);
             setTitle("Empty Page");
-            add(new JLabel("This page is empty because the value 'loginNextPage' has not been supplied. \n Check README.md for more information'"));
+            JPanel panel = new JPanel() {{
+                setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+                add(new JLabel("This page is empty because the value 'loginNextPage' has not been supplied."));
+                add(new JLabel("Check README.md for more information'"));
+            }};
+            add(panel);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             pack();
             setLocationRelativeTo(null); // Center the window
+            setSize(720, 500);
+            setPreferredSize(new Dimension(720, 500));
         }};
 
 
         JFrame emptySignupNextPage = new JFrame() {{
-            setSize(720, 500);
             setTitle("Empty Page");
-            add(new JLabel("This page is empty because the value 'signupNextPage' has not been supplied. \n Check README.md for more information'"));
+            JPanel panel = new JPanel() {{
+                setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+                add(new JLabel("This page is empty because the value 'signupNextPage' has not been supplied."));
+                add(new JLabel("Check README.md for more information'"));
+            }};
+            add(panel);
             setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             pack();
             setLocationRelativeTo(null); // Center the window
+            setSize(720, 500);
+            setPreferredSize(new Dimension(720, 500));
         }};
 
         Signup signup = new Signup(
